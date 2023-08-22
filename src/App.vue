f0a897f506dafbffab6bfa292eca4f030967be05
ColluraV <valerio.18.02.89@hotmail.it>
2023-08-22 16:07:31 +0200

    inizializzato

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import TheHeader from "./components/TheHeader.vue";
+/*import TheFooter from "./components/TheFooter.vue";*/
+
+export default {
+  components: {
+    TheHeader,
+    /*TheFooter,*/
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+<TheHeader/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+  data() {
+    return {
+      navLinks: [
+        { name: "CHARACTERS", path: "/" },
+        { name: "COMICS", path: "/products" },
+        { name: "MOVIES", path: "/contacts" },
+        { name: "TV", path: "/about" },
+        { name: "GAMES", path: "/about" },
+        { name: "COLLECTABLES", path: "/about" },
+        { name: "VIDEOS", path: "/about" },
+        { name: "FANS", path: "/about" },
+        { name: "NEWS", path: "/about" },
+        { name: "SHOP", path: "/about" },
+      ],
+    };
+  },
+};
+
+</script>
+
+<template>
+  <header>
+
+    <div class="container d-flex justify-content-between ">
+
+      <div>        <!-- //////logo////// -->
+        <img src="../assets/dc-logo.png" alt="">
+      </div>
+
+      <div>
+        <ul class="nav ">
+      <li class="nav-item" v-for="link in navLinks">
+        <a
+          href="#"
+          class="nav-link"
+          :class="{ active: link.name === 'Servizi' }"
+        >
+          {{ link.name }}
+        </a>
+      </li>
+    </ul>
+      </div>
+
+    </div>
+
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+</style>
